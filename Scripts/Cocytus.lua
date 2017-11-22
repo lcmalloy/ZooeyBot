f95970c7569530e96cab2e8ab5c83616e18d206f
lcmalloy <lcmalloy@gmail.com>
2017-11-22 09:37:40 -0800

    Update Cocytus.lua

--- Scripts/Cocytus.lua
+++ Scripts/Cocytus.lua
@@ -3,6 +3,8 @@ if battle < 3 then
 else
   EnableChargeAttack()
 end
+character_2:UseSkill(2)
+
 if battle == 1 then
 end
 if battle == 2 then
@@ -12,7 +14,6 @@ if battle == 3 then
   character_1:UseSkill(3)
   character_1:UseSkill(4)
   character_2:UseSkill(1)
-  character_2:UseSkill(2)
   character_3:UseSkill(2)
   character_3:UseSkill(1)
   character_4:UseSkill(1)
