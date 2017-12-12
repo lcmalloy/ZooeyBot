b8ac0742e6baf796394929f2493efaa66b7fb425
lcmalloy <lcmalloy@gmail.com>
2017-12-11 23:37:26 -0800

    wind.lua

---
+++ Scripts/wind.lua
@@ -0,0 +1,23 @@
+if battle == 1 and turn == 1 then
+end
+if battle == 1 and turn == 2 then
+end
+if battle == 2 and turn == 1 then
+end
+if battle == 2 and turn == 2 then
+end
+if battle == 3 and turn == 1 then
+  character_1:UseSkill(2)
+  character_1:UseSkill(3)
+  character_1:UseSkill(4)
+  character_2:UseSkill(1)
+  character_3:UseSkill(1)
+  character_3:UseSkill(2)
+  character_3:UseSkill(4)
+  character_4:UseSkill(3)
+  character_4:UseSkill(1)
+end
+if battle == 3 and turn == 2 then
+end
+if battle == 3 and turn == 3 then
+end
