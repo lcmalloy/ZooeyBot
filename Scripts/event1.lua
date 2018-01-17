7ea57ac2d750d9c7c5137adf36d7c255730d5815
lcmalloy <lcmalloy@gmail.com>
2018-01-17 04:12:20 -0800

    Update event1.lua

--- Scripts/event1.lua
+++ Scripts/event1.lua
@@ -1,15 +1,30 @@
 if battle == 1 and turn == 1 then
+  Summon(6)
   character_1:UseSkill(2)
   character_1:UseSkill(3)
   character_1:UseSkill(4)
-  character_1:UseSkill(1)
-  character_2:UseSkill(1)
+  character_2:UseSkill(4)
   character_2:UseSkill(2)
   character_2:UseSkill(3)
-  character_3:UseSkill(3)
+  character_2:UseSkill(1)
+  character_3:UseSkill(1)
+  character_3:UseSkill(2)
+  character_3:UseSkill(4)
   character_4:UseSkill(3)
   character_4:UseSkill(2)
   character_4:UseSkill(1)
 end
 if battle == 1 and turn == 2 then
+end
+if battle == 1 and turn == 3 then
+end
+if battle == 1 and turn == 4 then
+end
+if battle == 1 and turn == 5 then
+end
+if battle == 1 and turn == 6 then
+end
+if battle == 1 and turn == 7 then
+end
+if battle == 1 and turn == 8 then
 end
