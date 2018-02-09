f1c55db99e97b3b674627c71cf876b8f20431dba
lcmalloy <lcmalloy@gmail.com>
2018-02-09 10:21:33 -0800

    Update and rename event1.lua to event2.lua

--- Scripts/event2.lua
+++ Scripts/event2.lua
@@ -1,18 +1,14 @@
 if battle == 1 and turn == 1 then
-  Summon(6)
   character_1:UseSkill(2)
-  character_1:UseSkill(3)
   character_1:UseSkill(4)
-  character_2:UseSkill(4)
-  character_2:UseSkill(2)
-  character_2:UseSkill(3)
+  character_1:UseSkill(1)
   character_2:UseSkill(1)
+  character_2:UseSkill(2)
   character_3:UseSkill(1)
   character_3:UseSkill(2)
-  character_3:UseSkill(4)
-  character_4:UseSkill(3)
   character_4:UseSkill(2)
   character_4:UseSkill(1)
+  character_4:UseSkill(3)
 end
 if battle == 1 and turn == 2 then
 end
@@ -28,3 +24,6 @@ if battle == 1 and turn == 7 then
 end
 if battle == 1 and turn == 8 then
 end
+if battle == 1 and turn == 9 then
+end
+if battle == 1 and turn == 10 then
