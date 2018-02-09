03babee1986fe26d949c61c253444aa91c8daea1
lcmalloy <lcmalloy@gmail.com>
2018-02-09 09:19:01 -0800

    Update and rename wind.lua to fenrir.lua

--- Scripts/fenrir.lua
+++ Scripts/fenrir.lua
@@ -1,21 +1,18 @@
 if battle == 1 and turn == 1 then
+  character_3:UseSkill(1)
 end
 if battle == 1 and turn == 2 then
 end
 if battle == 2 and turn == 1 then
-end
-if battle == 2 and turn == 2 then
+  character_2:UseSkill(2)
+  character_2:UseSkill(1)
 end
 if battle == 3 and turn == 1 then
   character_1:UseSkill(2)
-  character_1:UseSkill(3)
-  character_1:UseSkill(4)
-  character_2:UseSkill(1)
-  character_3:UseSkill(1)
   character_3:UseSkill(2)
-  character_3:UseSkill(4)
-  character_4:UseSkill(3)
+  character_4:UseSkill(2)
   character_4:UseSkill(1)
+  character_4:UseSkill(3)
 end
 if battle == 3 and turn == 2 then
 end
