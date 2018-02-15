d126e5df23cc624fe849921c6c2485ba951e6062
lcmalloy <lcmalloy@gmail.com>
2018-02-15 11:07:08 -0800

    Update and rename darkgw.lua to Dhalo1.lua

---
+++ Dhalo1.lua
@@ -0,0 +1,64 @@
+if battle < 5 then
+  DisableChargeAttack()
+else
+  EnableChargeAttack()
+end  
+if character_5.name == "Katalina" then
+  characters_5:UseSkill(3)
+  characters_5:UseSkill(2)
+  characters_5:UseSkill(1)
+end
+if character_6.name == "Charlotta" then
+  characters_6:UseSkill(3)
+  characters_6:UseSkill(2)
+  characters_6:UseSkill(1)
+if battle == 1 and turn == 1 then
+  character_1:UseSkill(1)
+  character_1:UseSkill(2)
+  character_1:UseSkill(3)
+  character_1:UseSkill(4)
+  character_2:UseSkill(1)
+  character_2:UseSkill(3)
+  character_3:UseSkill(1)
+  character_3:UseSkill(2)
+  character_3:UseSkill(3)
+end
+if battle == 1 and turn == 2 then
+end
+if battle == 1 and turn == 3 then
+end
+if battle == 1 and turn == 4 then
+end
+if battle == 1 and turn == 5 then
+  character_4:UseSkill(1)
+  character_4:UseSkill(2)
+end
+if battle == 1 and turn == 6 then
+  character_1:UseSkill(2)
+  character_1:UseSkill(4)
+  character_4:UseSkill(3)
+  character_2:UseSkill(2)
+  character_3:UseSkill(1)
+end
+if battle == 1 and turn == 7 then
+  character_5:UseSkill(1)
+  character_5:UseSkill(2)
+  character_5:UseSkill(3)
+end
+if battle == 1 and turn == 8 then
+  character_1:UseSkill(1)
+  character_1:UseSkill(3)
+  character_2:UseSkill(1)
+  character_2:UseSkill(3)
+end
+if battle == 1 and turn == 9 then
+  summon(5)
+end
+if battle == 1 and turn == 10 then
+  summon(6)
+if battle == 1 and turn == 11 then
+end
+if battle == 1 and turn == 12 then
+end
+  
+
